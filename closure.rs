ef310622fbd52b9343e90251ba9fec1ace4a289d
gracheva <gracheva@tutu.tech>
2022-06-26 00:48:48 +0300

    Initial commit

---
+++ closure.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let str1 = String::from("hello");
+    let test = | mut str: String | {
+        str.push_str(&String::from(" world"));  
+        str
+    };
+    
+    let tested = test(str1);
+
+    println!("{:?}", tested);
+}
